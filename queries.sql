adce53432bdafe7e54bc041c428c0a029e6810f6
silvodesigns <kevsilvo@gmail.com>
2020-01-13 00:18:56 -0500

    solved second sql riddle

--- queries.sql
+++ queries.sql
@@ -2,6 +2,10 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT ProductName, CategoryName FROM Product
+Join  Category 
+on Product.CategoryId = Category.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
